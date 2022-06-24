e7e64e5b4adf5ee6c41aef80eb8a32f0fe0cf810
Sean Fraser <7241905+TheSeanFraser@users.noreply.github.com>
2022-06-24 13:23:19 -0400

    Created maps for fall

--- src/map_maker.py
+++ src/map_maker.py
@@ -13,7 +13,7 @@ ontario_shape = gpd.read_file(config.res_dir + "ontario_2011.json")
 print("Ontario shape loaded!")
 
 
-def make_maps():
+def make_spring_maps():
     global ontario_shape
     directory = config.species_data_dir + "20_YEARS_SPRING_PEAKS\\"
 
@@ -76,6 +76,69 @@ def make_maps():
         plt.close()
 
 
+def make_fall_maps():
+    global ontario_shape
+    directory = config.species_data_dir + "20_YEARS_FALL_PEAKS\\"
+
+    # Loop through all of the species to build a dataframe
+    for filename in os.listdir(directory):
+        single_data = pd.read_csv(directory + filename, header=1)
+        species = re.search("(.*).csv", filename).group(1)
+        print("Making map for: " + species)
+
+        # Merges the dataframes with the matching region names column
+        merged = ontario_shape.set_index("CDNAME").join(
+            single_data.set_index("Unnamed: 0"))
+
+        # Sort the date and change to easy format
+        merged.sort_values(species, inplace=True)
+        merged[species] = pd.to_datetime(merged[species], format="%Y-%m-%d")
+        merged[species] = pd.to_datetime(merged[species]).dt.strftime('%m-%d')
+
+
+        # Define the map plot
+        fig, ax = plt.subplots(1, figsize=(12, 8))
+        ax.axis('off')
+        ax.set_title(species + ' Fall Mass Arrival Dates in Ontario',
+                     fontdict={'fontsize': '20', 'fontweight': '3'})
+
+        # Add a watermark
+        ax.text(0, 0, "Sean Fraser - 2022", transform=ax.transAxes,
+            fontsize=10, color='gray', alpha=0.5,
+            ha='left', va='bottom')
+
+        # "Missing values" will not plot anything if there are no missing values
+        # so there needs to be a check
+        if merged[str(species)].isnull().values.any():
+            merged.plot(column=species,
+                        cmap=matplotlib.cm.get_cmap('inferno_r'),
+                        linewidth=0.9,
+                        ax=ax,
+                        edgecolor='1',
+                        legend=True, legend_kwds={'loc': 'center right',
+                                                  'bbox_to_anchor': (1.5, 0.5),
+                                                  'ncol': 2,
+                                                  'fontsize': 'large'},
+                        missing_kwds={
+                    "color": "lightgrey",
+                    "label": "No data", }, )
+        else:
+            merged.plot(column=str(species),
+                        cmap=matplotlib.cm.get_cmap('inferno_r'),
+                        linewidth=0.9,
+                        ax=ax,
+                        edgecolor='1',
+                        legend=True, legend_kwds={'loc': 'center right',
+                                                  'bbox_to_anchor': (1.5, 0.5),
+                                                  'ncol': 2,
+                                                  'fontsize': 'large'})
+
+        plt.subplots_adjust(left=-0.1)
+        # plt.tight_layout()
+        fig.savefig(config.proj_path + "media\\maps\\fall\\" + species + ".png", dpi=100)
+        plt.close()
+
+
 def load_ontario_shape():
     print("Loading Ontario shape")
     ontario_shape = gpd.read_file(config.res_dir + "ontario_2011.json")
@@ -83,4 +146,5 @@ def load_ontario_shape():
     return ontario_shape
 
 
-make_maps()
+# make_spring_maps()
+make_fall_maps()
