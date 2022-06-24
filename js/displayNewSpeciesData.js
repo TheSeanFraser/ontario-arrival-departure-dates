58aeef898f582545ec485b8a23368ff7f74a7f80
Sean Fraser <7241905+TheSeanFraser@users.noreply.github.com>
2022-06-24 13:31:03 -0400

    Added fall maps to website

--- js/displayNewSpeciesData.js
+++ js/displayNewSpeciesData.js
@@ -1,14 +1,18 @@
 'use strict';
 
-var path = 'https://theseanfraser.github.io/ontario-arrival-departure-dates/media/maps/spring/';
+var spring_path = 'https://theseanfraser.github.io/ontario-arrival-departure-dates/media/maps/spring/';
+var fall_path = 'https://theseanfraser.github.io/ontario-arrival-departure-dates/media/maps/fall/';
 var selectSpeciesButton = document.getElementById("selectSpeciesButton");
 
 selectSpeciesButton.onclick = function() {
 	console.log("Button clicked");
 	var species = document.getElementById("speciesSelector").value;
 
-    var map_img = document.getElementById("map_img");
-    map_img.src = path + species + ".png";
+    var spring_map_img = document.getElementById("spring_map_img");
+    var fall_map_img = document.getElementById("fall_map_img");
+    
+    spring_map_img.src = spring_path + species + ".png";
+    fall_map_img.src = fall_path + species + ".png";
 
 };
 
